f6583c70ed0474b1aa154f3b0aadf9bd0ae15a65
abc <abc@example.com>
2022-01-24 20:00:45 -0700

    finished

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,13 +3,14 @@ const { restricted } = require('./../auth/auth-middleware')
 const router = require('express').Router()
 const Users = require('./users-model')
 
-router.get('/', restricted, async (req, res, next) => {
-  try {
-    const [ user_id ] = await Users.find()
-    res.json(user_id)
-  } catch (err) {
+router.get('/', restricted, (req, res, next) => {
+  Users.find()
+  .then(user => {
+    res.json(user)
+  })
+  .catch(err => {
     next(err)
-  }
+  })
 })
 
 
